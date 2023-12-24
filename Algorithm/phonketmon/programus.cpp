bc7fb25366bf0a14d689c2f5b13ec21fc112cd37
minwn3318 <minwn3318@naver.com>
2023-12-24 09:29:04 +0900

    Update programus.cpp

--- Algorithm/phonketmon/programus.cpp
+++ Algorithm/phonketmon/programus.cpp
@@ -2,29 +2,50 @@
 #include <vector>
 using namespace std;
 
-vector<int> input = { 1,1,2,2 };
+vector<int> input = { 3,3,3,4,3,4 };
 
 int solution(vector<int> nums)
 {
-    int count = 1;
-    vector<int>::iterator input_Iter1;
-    for (input_Iter1 = nums.begin(); input_Iter1 < nums.end(); input_Iter1++)
+    int len = nums.size();
+    int choose = (nums.size()/2);
+    int count = 0;
+    int differnt = 0;
+    int answer;
+
+    int iter1_el = 0;
+    int iter2_el = 0;
+
+    vector<int>::iterator input_Iter = nums.begin();
+
+    while(count < (nums.size() - 1))
     {
-        vector<int>::iterator input_Iter2;
-        //for(input_Iter2 = nums[count]; )
+        iter1_el = *input_Iter;
+        input_Iter++;
+        iter2_el = *input_Iter;
+        printf("%d \n", iter1_el);
+        printf("%d \n", iter2_el);
+        if (iter1_el != iter2_el)
+        {
+            differnt++;
+        }
+        count++;
     }
-    int answer = 0;
+
+    if (differnt >= choose)
+    {
+        answer = choose;
+    }
+    else
+    {
+        answer = differnt + 1;
+    }
     return answer;
 }
 
 int main()
 {
-    vector<int>::iterator input_Iter = input.begin();
-    printf("%x \n", &(*input_Iter));
-    input_Iter++;
-    printf("%x \n", &(*input_Iter));
-    input_Iter++;
-    printf("%x \n", &(*input_Iter));
+
+    printf("결과 : %d", solution(input));
 
     return 0;
 }
