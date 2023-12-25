cfa05a0393de253664a03535e97bae440163f7b9
minwn3318 <minwn3318@naver.com>
2023-12-25 15:47:10 +0900

    Create procession.cpp

---
+++ Algorithm/procession/procession.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> a = { {1, 2}, {3, 4} };
+vector<vector<int>>::iterator b = a.begin();
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    return answer;
+}
+
+// 현재 벡터의 2차원 배열을 어떻게 순회해야할 지 모르겠음. 반복자를 활용해 보는데
+// 반복자의 *연산이 vecotr에는 안됨
+// 인덱스로 해봐야 겠음 
+int main()
+{
+	return 0;
+}
